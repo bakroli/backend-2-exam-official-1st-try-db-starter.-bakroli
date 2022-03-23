3affd0b9483c702d66b157dc08afd85474ba3e3f
bakroli <bak.roland@hotmail.com>
2022-03-23 12:42:50 +0100

    half

--- src/main/java/hu/nive/ujratervezes/albums/AlbumManager.java
+++ src/main/java/hu/nive/ujratervezes/albums/AlbumManager.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.albums;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlbumManager {
 
     private final String dbUrl;
@@ -11,4 +15,28 @@ public class AlbumManager {
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
     }
+
+    public List<String> getSingersWithTheirLatestAlbum() {
+        List<String> result = new ArrayList<>();
+        String sqlQuery = "SELECT DISTINCT singer_mame, release_year, singer_name || ' - ' || release_year FROM singers RIGHT JOIN albums ON singers.singer_id = albums.singer_id ORDER BY release_year DESC";
+
+        try (Connection connection = DriverManager.getConnection(dbUrl,dbUser,dbPassword)) {
+            PreparedStatement statement = connection.prepareStatement(sqlQuery);
+            result = getResult(statement.executeQuery());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>(result);
+
+    }
+
+    private static List<String> getResult(ResultSet resultSet) throws SQLException {
+        List<String> result = new ArrayList<>();
+        while (resultSet.next()) {
+            result.add(resultSet.getString(2));
+        }
+        return result;
+    }
+
+
 }
